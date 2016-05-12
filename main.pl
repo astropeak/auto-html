a8002580bb7000e2d4d7c414c513ce8595fe4f03
Astropeak Luo <astropeak@163.com>
2016-05-12 17:04:33 +0800

    link convert input with divide part.
    But there are inline space problems

--- main.pl
+++ main.pl
@@ -1,7 +1,9 @@
 # push(@INC, "/home/astropeak/Dropbox/project/aspk-code-base/perl");
 use Aspk::HtmlElement;
+use ConvertInput qw(build_element_tree);
+use Aspk::debug qw(printHash);
 
-sub _divide_element_vertically{
+sub _divide_element_horizonally{
     my ($element, $len) =@_;
     foreach (@{$len}) {
         my $e = Aspk::HtmlElement->new({tag=>"div", parent=>$element});
@@ -9,10 +11,10 @@ sub _divide_element_vertically{
         $e->style(width,"100%");
     }
 
-    # return $element->prop(children);
+    return $element->prop(children);
 }
 
-sub _divide_element_horizonally{
+sub _divide_element_vertically{
     my ($element, $len) =@_;
     foreach (@{$len}) {
         my $e = Aspk::HtmlElement->new({tag=>"div", parent=>$element});
@@ -21,12 +23,15 @@ sub _divide_element_horizonally{
         $e->style(display, "inline-block");
     }
 
-    # return $element->prop(children);
+    return $element->prop(children);
 }
 
 
 sub divide_element {
     my ($element, $oritation, $len) =@_;
+    print "Enter divide $oritation\n";
+    printHash($len);
+
     if ($oritation eq "horizonally") {
         return _divide_element_horizonally($element, $len);
     } else {
@@ -34,25 +39,79 @@ sub divide_element {
     }
 }
 
+
+
+sub aaaaa{
+    my ($element, $input_tree)= @_;
+
+    print "Enter aaaaa. element:\n";
+    printHash($element);
+    print "input_tree:\n";
+    printHash($input_tree);
+
+    my $children = $input_tree->prop(children);
+    print "length of children: ".scalar(@{$children})."\n";
+
+    if (scalar(@{$children})>0) {
+        print "here\n";
+        my @width = map {$_->prop(data)->{width}} @{$children};
+        my @height = map {$_->prop(data)->{height}} @{$children};
+
+        if ($width[0] == $input_tree->prop(data)->{width}) {
+            divide_element($element, "horizonally", \@height);
+        } else {
+            @width = map {$_ - 8} @width;
+            divide_element($element, "vertically", \@width);
+        }
+
+        my $total = scalar(@{$children});
+        for(my $i=0; $i<$total; $i++) {
+            print "i: $i, total: $total\n";
+            aaaaa($element->prop(children)->[$i], $children->[$i]);
+        }
+    } else {
+        print "in else\n";
+    }
+    print "Exit aaaaa\n";
+}
+
+
+
+
+
+
+
 my $h = Aspk::HtmlElement->new({tag=>"html"});
 my $b = Aspk::HtmlElement->new({tag=>"body", parent=>$h});
 
 my $e=Aspk::HtmlElement->new({tag=>"div", prop=>{id=>"wrapper"}, parent=>$b});
-$e->style("height", "300px");
-$e->style("width", "600px");
+$e->style("height", "100");
+$e->style("width", "300");
 # $e->print();
 
-print "\nDivide the element:\n";
-# divide_element($e, "vertically", ["30%", "60%", "10%"]);
-divide_element($e, "horizonally", ["30%", "50%", "10%"]);
-# $e->print();
 
-my $tc=Aspk::HtmlElement->new({tag=>"text", prop=>{content=>"I am a connect"}, parent=>$e->prop(children)->[0]});
-# $e->prop(children)->[0]->add_child($tc);
+# print "\nDivide the element:\n";
+# # divide_element($e, "vertically", ["30%", "60%", "10%"]);
+# divide_element($e, "horizonally", ["30%", "50%", "10%"]);
+# # $e->print();
 
-divide_element($e->prop(children)->[1], "vertically", ["30%", "40%"]);
+# my $tc=Aspk::HtmlElement->new({tag=>"text", prop=>{content=>"I am a connect"}, parent=>$e->prop(children)->[0]});
+my $tc=Aspk::HtmlElement->new({tag=>"text", prop=>{content=>"I am a connect"}});
+# # $e->prop(children)->[0]->add_child($tc);
 
-print "\nHtml:\n";
+# divide_element($e->prop(children)->[1], "vertically", ["30%", "40%"]);
+
+# print "\nHtml:\n";
+
+
+
+
+my $input_tree = build_element_tree("./input");
+aaaaa($e, $input_tree);
+# print "herer after aaaaa\n";
+# printHash($e);
+
+
 
 
 $h->traverse({prefunc=> sub
@@ -64,11 +123,11 @@ $h->traverse({prefunc=> sub
                       ->style('padding', 0)
                       ->style('margin-left',"-0px")
                       ->style('vertical-align',"top")
-                      ->style('overflow','auto')
+                      ->style('overflow','hidden')
                       # ->style('font-size',0)
                       if ($e->prop(tag) eq 'div');
 
-                  $e->add_child($tc) if ($e->prop(tag) eq 'div');
+                  $e->add_child($tc) if ($e->prop(tag) eq 'div') && (scalar(@{($e->prop(children))}) == 0);
               }});
 
 $rst = $h->format();
